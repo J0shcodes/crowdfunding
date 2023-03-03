d1a66f061ace22b81c6204b2401aafce109604e1
J0shcodes <mosesjoshua350@gmail.com>
2023-03-03 09:03:40 +0100

    Cleaned up code and updated README file

--- README.md
+++ README.md
@@ -1,13 +1,12 @@
 # Sample Hardhat Project
 
-This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, and a script that deploys that contract.
+This project demonstrates a basic Hardhat use case. It comes with a sample crowdfunding smart contract, a test for the smart contract, and a script that deploys the smart contract.
 
-Try running some of the following tasks:
+To install the necessary dependencies, in your terminal pointing to the root folder run:
 
 ```shell
-npx hardhat help
-npx hardhat test
-REPORT_GAS=true npx hardhat test
-npx hardhat node
-npx hardhat run scripts/deploy.js
+yarn install
 ```
+
+## Smart Contract Address
+0xc6b26C6CC673DF83BA051871b8140cE9ff61859f
--- dapp/src/App.js
+++ dapp/src/App.js
@@ -1,40 +1,18 @@
 import { useCelo } from "@celo/react-celo";
 import ContributeModal from "./contributeModal";
-import { useState, useEffect, useCallback } from "react";
+import { useState } from "react";
 import BigNumber from "bignumber.js";
-import { abi } from "./crowdfunding.abi";
 
 function App() {
-  const { connect, address, kit, getConnectedKit, performActions } = useCelo();
+  const { connect, address, kit, getConnectedKit } = useCelo();
   const ERC20_DECIMALS = 18;
   const contractAddress = "0xc6b26C6CC673DF83BA051871b8140cE9ff61859f";
 
   const [totalBalance, setTotalBalance] = useState("0.00");
-  const [totalContribution, setTotalContribution] = useState("");
 
   const [openContributeModal, setOpenContributeModal] = useState(false);
 
-  // const getContractBalance = useCallback(async () => {
-  //   const contract = await instantiateContract();
-  //   const contractBalance = contract.methods.viewBalance().call();
-  //   return contractBalance;
-  // }, []);
-
-  // useEffect(() => {
-  //   const getBalance = async () => {
-  //     const contributions = await getContractBalance();
-  //     setTotalContribution(contributions);
-  //   };
-  //   if (address) {
-  //     getBalance();
-  //   }
-  // }, [getContractBalance, address]);
-
   async function getStableToken() {
-    // const accounts = await kit.contracts.getAccounts();
-    // console.log(accounts);
-    // // const summary = accounts.getAccountSummary(address);
-    // return accounts.connection.getBalance(address);
     const kit = await getConnectedKit();
     const cUSDToken = await kit.contracts.getStableToken();
     const usercUSDBalance = await cUSDToken.balanceOf(address);
@@ -47,7 +25,6 @@ function App() {
       .shiftedBy(-ERC20_DECIMALS)
       .toFixed(2);
     console.log(cUSDBalance);
-    // console.log(cUSDBalance.shiftedBy(-ERC20_DECIMALS).toFixed(2));
     setTotalBalance(cUSDBalance);
   };
 
@@ -55,37 +32,9 @@ function App() {
     accountSummary();
   }
 
-  // accountSummary();
-
-  // const instantiateContract = async () => {
-  //   const kit = await getConnectedKit();
-  //   const contract = new kit.connection.web3.eth.Contract(abi, contractAddress);
-  //   console.log(contract);
-  //   return contract;
-  // };
-
-  const transfer = async () => {
-    await performActions(async (kit) => {
-      const celoTokenContract = await kit.contracts.getStableToken();
-      console.log(celoTokenContract);
-      const tx = await celoTokenContract
-        .transfer(contractAddress, 100)
-        .send({ from: address });
-      const receipt = await tx.waitReceipt();
-      console.log("Transaction Receipt: ", receipt);
-    });
-  };
-
-  // transfer();
-
-  // const contribute = async (amount) => {
-  //   const contract = await instantiateContract();
-  //   contract.methods.contribute(amount).send({ from: address });
-  // };
-
   return (
     <div className="bg-[url(https://images.pexels.com/photos/3943716/pexels-photo-3943716.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2)] bg-cover bg-center bg-no-repeat">
-      {/* <div className="text-end p-4">cUSD: {totalBalance}</div> */}
+      <div className="text-end p-4">cUSD: {totalBalance}</div>
       <div className="flex flex-col justify-center items-center h-screen relative px-8">
         <div className="text-center bg-[#7b7a82] text-white text-xl p-2">
           <span>Welcome to our crowdfunding Dapp, where anyone can invest in
